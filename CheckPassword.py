698cbe855245510731192a6166c249e4fd8fe47e
kubo11 <jakubmatejka77@gmail.com>
2026-04-23 08:37:01 +0200

    Update - kontrola dlzky hesla
    program kontroluje aj dlzku hesla (min. 8  znakov)

--- CheckPassword.py
+++ CheckPassword.py
@@ -1,4 +1,5 @@
 # sem doplnte link na svoj kod
+# https://github.com/kubik11111/sachovnica
 
 import os;
 
@@ -6,6 +7,7 @@ def check_password(password: str) -> bool:
     small_letter = False
     capital_letter = False
     number = False
+    length = False
 
     for c in password:
         if(c.isdigit()):
@@ -14,8 +16,10 @@ def check_password(password: str) -> bool:
             small_letter = True
         elif(c.isupper()):
             capital_letter = True
+        if len(password) >=8:
+            length = True
     
-    return small_letter and capital_letter and number
+    return small_letter and capital_letter and number and length
 
 
 dir = os.path.dirname(__file__)                     # Nacita cestu k suboru
